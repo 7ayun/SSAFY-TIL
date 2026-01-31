9ef9116af1687de3748d6d76f72a69367eed73b8
LEEMINGYU <wcentipede27@gmail.com>
2026-01-31 23:24:35 +0900

    Git 복구 전략 · 파일 시스템 · 생성형 AI 활용

---
+++ test.py
@@ -0,0 +1,8 @@
+if id == "" and password == "":
+    print("아이디와 비밀번호가 없습니다.")
+elif id == "" and password == "":
+    print("아이디가 없습니다.")
+elif id == "" and password == "":
+    print("비밀번호가 없습니다.")
+else:
+    print("로그인 성공")
